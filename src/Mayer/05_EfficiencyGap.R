f0885508a856df1b422a902e2e2907ded179204a
ckelling <ckelling@vt.edu>
2018-06-19 11:16:35 -0400

    adding mean median score and changing names

--- src/Mayer/05_EfficiencyGap.R
+++ src/Mayer/05_EfficiencyGap.R
@@ -11,13 +11,19 @@ wasted.vote <- function(x,y){
 }
 
 efficiency.gap <- function(df){
-  wasted.d.votes = Vectorize(wasted.vote)(df$Dem..Votes, df$Rep..Votes) #compute total wasted dem votes
-  wasted.r.votes = Vectorize(wasted.vote)(df$Rep..Votes, df$Dem..Votes) #compute total wasted rep votes
+  wasted.d.votes = Vectorize(wasted.vote)(df$Dem.Votes, df$Rep.Votes) #compute total wasted dem votes
+  wasted.r.votes = Vectorize(wasted.vote)(df$Rep.Votes, df$Dem.Votes) #compute total wasted rep votes
   return(sum(wasted.d.votes-wasted.r.votes)/sum(df)) #sum the difference and divide by total number of votes
   }
   
 #efficency.gap(df)
-  
+
+mean_med <- function(df){
+  mean_score <- mean(df$Dem.Votes/(df$Dem.Votes + df$Rep.Votes))
+  med_score <- median(df$Dem.Votes/(df$Dem.Votes + df$Rep.Votes))
+  mean_med <- mean_score- med_score
+  return(mean_med)
+}
   
   
   
